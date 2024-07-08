2e24e01da74e3661dcd3410db75f6863a50bbc13
Felipe Pereira <fdiaspp@gmail.com>
2024-07-08 11:12:58 -0300

    enabling execution of a specific job from definition

--- main.py
+++ main.py
@@ -4,20 +4,28 @@ import click
 import yaml
 
 
+
 @click.command()
 @click.option('-p', '--pipeline', required=True, help='The path to the pipeline YAML file.')
-def simple_pipeline(pipeline: str):
+@click.option('--job', help='The name of the job to execute.')
+def simple_pipeline(pipeline: str, job: str = None):
+    
     dag = DAG()
     with open(pipeline) as stream:
         try:
             pipeline = yaml.safe_load(stream)
-            for job in pipeline['jobs']:
-                dag.add(job=job, depends_on=pipeline['jobs'][job].get('depends_on', []),  job_args=[json.dumps(pipeline['jobs'][job])])
+            
+            if job:
+                dag.add(job=job, depends_on=[],  job_args=[json.dumps(pipeline['jobs'][job])])
+            else:
+                for job in pipeline['jobs']:
+                    dag.add(job=job, depends_on=pipeline['jobs'][job].get('depends_on', []),  job_args=[json.dumps(pipeline['jobs'][job])])
         
         except yaml.YAMLError as exc:
-            print(exc)
+            print(exc)    
     
     dag.execute()
+    
 
 
 if __name__ == "__main__":
